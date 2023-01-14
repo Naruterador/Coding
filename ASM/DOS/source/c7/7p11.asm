ed90e543d71ba25ee948425bf46db6a1c04e5651
Naruterador <103258937@qq.com>
2023-01-14 11:31:26 +0800

    update

--- ASM/DOS/source/c7/7p11.asm
+++ ASM/DOS/source/c7/7p11.asm
@@ -12,7 +12,33 @@ datasg segment
 datasg ends
 
 codesg segment
- start:
+  start: mov ax,stacksg
+         mov ss,ax
+         mov sp,16
+
+         mov ax,datasg
+         mov ds,ax
+         
+         mov bx,0
+         mov cx,4
+
+         s1: mov si,0
+             push cx
+             mov cx,4
+         
+         s2: mov al,ds:[bx+si+3]
+             and al,11011111B
+             mov ds:[bx+si+3],al
+             inc si
+             loop s2
+
+         add bx,16
+         pop cx
+         loop s1
+             
+    mov ax,4c00h
+    int 21h
+    
 codesg ends
 end start
 
